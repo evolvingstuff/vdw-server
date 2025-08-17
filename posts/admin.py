20b97c8ffe4d0b7ca528215f537ed8d847ac1483
Thomas Lahore <tom.lahore@gmail.com>
2025-08-17 12:13:31 -0700

    minor changes and requirements.txt

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django import forms
+from django.urls import reverse
+from django.utils.html import format_html
 from .models import Post, Tag
 
 
@@ -22,16 +24,17 @@ class TagAdmin(admin.ModelAdmin):
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     form = PostAdminForm
-    list_display = ['title', 'status', 'created_date', 'modified_date']
+    list_display = ['title', 'status', 'live_link', 'created_date', 'modified_date']
     list_filter = ['status', 'created_date', 'modified_date', 'tags']
     search_fields = ['title', 'content_md', 'meta_description']
     prepopulated_fields = {'slug': ('title',)}
     filter_horizontal = ['tags']
     date_hierarchy = 'created_date'
+    readonly_fields = ['live_link']
     
     fieldsets = (
         ('Content', {
-            'fields': ('title', 'slug', 'content_md', 'status')
+            'fields': ('title', 'slug', 'content_md', 'status', 'live_link')
         }),
         ('Tags', {
             'fields': ('tags',)
@@ -45,6 +48,16 @@ class PostAdmin(admin.ModelAdmin):
             'classes': ('collapse',)
         }),
     )
+    
+    def live_link(self, obj):
+        if obj.pk and obj.status == 'published':
+            url = reverse('post_detail', args=[obj.slug])
+            return format_html('<a href="{}" target="_blank">View Live →</a>', url)
+        elif obj.pk and obj.status == 'draft':
+            return "Publish to view live"
+        else:
+            return "Save first"
+    live_link.short_description = "Live URL"
     
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
